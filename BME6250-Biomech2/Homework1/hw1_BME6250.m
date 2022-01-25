b8e0d37e481c413556db461674f10727456d73f0
Anthony Le <anthony.h.le@utah.edu>
2022-01-25 16:24:24 -0700

    Pull data from febio sim hw1 prb2 bme 6250

--- BME6250-Biomech2/Homework1/hw1_BME6250.m
+++ BME6250-Biomech2/Homework1/hw1_BME6250.m
@@ -1,5 +1,5 @@
 % Created by: Anthony H. Le
-% Last updated: 2022-01-24
+% Last updated: 2022-01-25
 
 % BME 6250 - Biomechanics II
 % Homework 1
@@ -70,7 +70,7 @@ E = (1/2) * ((U * U) - 1);
 close all;
 clear;
 
-% define the four nodes of the element, El_0
+% define the four markers of the element, El_0
 P1_0 = [0 0 0];
 P2_0 = [0 1 0];
 P3_0 = [2 1 0];
@@ -84,7 +84,7 @@ P14_0 = P4_0 - P1_0;
 P13_0 = P3_0 - P1_0;
 P24_0 = P2_0 - P4_0;
 
-% define the four nodes of element, El_x
+% define the four markers of element, El_x
 P1_x = [0 0 0];
 P2_x = [0.3 1.3 0];
 P3_x = [1.8 1.4 0];
@@ -99,46 +99,36 @@ P13_x = P3_x - P1_x;
 P24_x = P2_x - P4_x;
 
 % define reference vectors of El, 6x3 matrix
-dX = [P12_0;
-     P23_0;
-     P34_0;
-     P14_0;
-     P13_0;
-     P24_0];
+dX = [P12_0; P23_0; P34_0; P14_0; P13_0; P24_0];
 
 % define deformed vectors of El, 6x3 matrix
-dx = [P12_x;
-     P23_x;
-     P34_x;
-     P14_x;
-     P13_x;
-     P24_x];
+dx = [P12_x; P23_x; P34_x; P14_x; P13_x; P24_x];
 
 % calculate the reference lengths of dX
 dS = vecnorm(dX, 2, 2); % 6x1 vector
 
-% calculate the deformed lengths of dX
+% or calculate the squared lengths of dX
+dS2 = dot(dX, dX, 2); % 6x1 vector
+
+% calculate the deformed lengths of dx
 ds = vecnorm(dx, 2, 2); % 6x1 vector
 
+% or calculate the squared lengths of dx
+ds2 = dot(dx, dx, 2); % 6x1 vector
+
 % calculate b, 6x1 vector
-b = ds.^2 - dS.^2;
+% b = ds.^2 - dS.^2;
+b = ds2 - dS2; 
 
 % calculate A, 6x3 matrix
 A = 2 .* [dX(:, 1).^2 dX(:, 2).^2 2.*dX(:, 1).*dX(:, 2)];
 
 % solve the system of equations to find the Green-Lagrange strain elements
-% of El_x: E_11, E_22, and E_12
+% of El_x, E_11, E_22, and E_12, as the least-squares solution
 E = A\b; % 3x1 vector
 
-% calcuate the displacement, u, between points of 
-% X (reference) and x (deformed) 
-% u = dx - dX;
-
-% calculate the deformation gradient
-% F = 1 + u;
-
-% calculate the Green-Lagrange strain tensor
-% E = (1/2) * ((transpose(F) * F) - 1);
-
 %% Problem 5
+close all;
+clear;
+
 
